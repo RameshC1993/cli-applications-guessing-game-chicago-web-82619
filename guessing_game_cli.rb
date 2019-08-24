998627ffe74a323f2b2d8599f5a95ddd22791af5
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-24 20:46:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,9 +6,9 @@ def run_guessing_game
   
   if guess == "exit"
     puts "Goodbye!"
-  elsif guess != rand_num.to_s
-    puts "Sorry! The computer guessed 6."
-  else
+  elsif guess == rand_num.to_s
     puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed 6."
   end
 end
